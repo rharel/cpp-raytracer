789dba1c8bdcde596a655ec8d9a5dcefdcac6e3a
Raoul Harel <raoul.harel@gmail.com>
2016-06-07 23:01:01 +0200

    added vector and matrix types

---
+++ core/include/iris/MatrixTypes.h
@@ -0,0 +1,22 @@
+#ifndef IRIS_MATRIX_TYPES_H
+#define IRIS_MATRIX_TYPES_H
+/**
+ * @file MatrixTypes.h
+ * @author Raoul Harel
+ * @url github.com/rharel/cpp-pathtracer
+ *
+ * Matrix type definitions.
+ */
+
+
+#include <glm/mat4x4.hpp>
+
+
+namespace iris
+{
+    typedef glm::mat4 Matrix4;
+}
+
+
+
+#endif  // IRIS_MATRIX_TYPES_H
---
+++ core/include/iris/VectorTypes.h
@@ -0,0 +1,24 @@
+#ifndef IRIS_VECTOR_TYPES_H
+#define IRIS_VECTOR_TYPES_H
+/**
+ * @file VectorTypes.h
+ * @author Raoul Harel
+ * @url github.com/rharel/cpp-pathtracer
+ *
+ * Vector type definitions.
+ */
+
+
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+
+
+namespace iris
+{
+    typedef glm::vec2 Vector2;
+    typedef glm::vec3 Vector3;
+}
+
+
+
+#endif  // IRIS_VECTOR_TYPES_H
