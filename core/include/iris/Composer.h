ae8108ba9ed5ff7af2a0ed1cee81ee70d24b36f9
Raoul Harel <raoul.harel@gmail.com>
2016-06-13 20:26:24 +0200

    added Composer class

---
+++ core/include/iris/Composer.h
@@ -0,0 +1,85 @@
+#ifndef IRIS_COMPOSER_H
+#define IRIS_COMPOSER_H
+/**
+ * @author Raoul Harel
+ * @url github.com/rharel/cpp-raytracer
+ *
+ * Composer class.
+ */
+
+
+#include <iris/Scene.h>
+#include <iris/Camera.h>
+#include <iris/Image.h>
+
+#include <iris/Ray.h>
+#include <iris/Raytracer.h>
+#include <iris/Pathtracer.h>
+
+#include <iris/Sampler.h>
+#include <iris/NaiveSampler.h>
+
+#include <iris/vector_types.h>
+
+
+namespace iris
+{
+    /**
+     * The composer takes a camera, tracer, and scene, and uses those to
+     * render an image.
+     */
+    class Composer
+    {
+        public:
+        /**
+         * Creates default composer.
+         */
+        Composer
+        (
+            size_t image_width, size_t image_height,
+            const Scene& scene, const Camera& camera
+        );
+        /**
+         * Creates composer with given sampler/tracer.
+         */
+        Composer
+        (
+            size_t image_width, size_t image_height,
+            const Scene& scene, const Camera& camera,
+            Sampler& sampler, const Raytracer& tracer
+        );
+
+        /*
+         * Renders a traced image.
+         */
+        void render();
+
+        /**
+         * Gets image.
+         */
+        const Image& image() const;
+
+        private:
+        static NaiveSampler default_sampler;
+        static Pathtracer default_tracer;
+
+        /**
+         * Renders a pixel.
+         */
+        void render_pixel(size_t x, size_t y);
+        /**
+         * Makes a ray that passes through given pixel.
+         */
+        Ray ray_through(float x, float y) const;
+
+        Image image_;
+        Sampler* sampler_;
+
+        const Scene* scene_;
+        const Camera* camera_;
+        const Raytracer* tracer_;
+    };
+}
+
+
+#endif  // IRIS_COMPOSER_H
---
+++ core/src/Composer.cpp
@@ -0,0 +1,94 @@
+#include <iris/Composer.h>
+#include <iris/sample_aggregators.h>
+
+#include <vector>
+
+
+using namespace iris;
+
+
+NaiveSampler Composer::default_sampler;
+Pathtracer Composer::default_tracer(3);
+
+Composer::Composer
+(
+    const size_t width, const size_t height,
+    const Scene& scene, const Camera& camera
+)
+    : Composer
+(
+    width, height, 
+    scene, camera, 
+    default_sampler, default_tracer
+) {}
+
+Composer::Composer
+(
+    const size_t width, const size_t height,
+    const Scene& scene, const Camera& camera,
+    Sampler& sampler, const Raytracer& tracer
+)
+    : image_(width, height), 
+      scene_(&scene), camera_(&camera), 
+      sampler_(&sampler), tracer_(&tracer) {}
+
+void Composer::render()
+{
+    for (size_t x = 0; x < image().width(); ++x)
+    {
+        for (size_t y = 0; y < image().height(); ++y)
+        {
+            render_pixel(x, y);
+        }
+    }
+}
+void Composer::render_pixel
+(
+    const size_t x, 
+    const size_t y
+)
+{   
+    const Vector2 origin(x, y);
+    std::vector<Vector2> samples;
+    std::vector<Vector3> colors;
+
+    sampler_->reset();
+    while (sampler_->has_next())
+    {
+        const Vector2 sample = sampler_->next();
+        const Vector2 target = origin + sample;
+        const Ray ray = ray_through(target.x, target.y);
+        const Vector3 color = tracer_->trace(ray, *scene_);
+        sampler_->report(color);
+
+        samples.push_back(sample);
+        colors.push_back(color);
+    }
+
+    image_.pixel(x, y, aggregate::uniform(colors, samples));
+}
+Ray Composer::ray_through(const float x, const float y) const
+{
+    // Pixel in normalized window space
+    const Vector2 pn
+    (
+        x / static_cast<float>(image().width()),
+        y / static_cast<float>(image().height())
+    );
+    // Pixel in camera space
+    const Vector3 pc = camera_->on_near(pn.x, pn.y);
+    // Pixel in world space
+    const Vector3 p(camera_->view_matrix() * Vector4(pc, 1));
+
+    // Ray origin in world space
+    const Vector3 o = camera_->position();
+    // Ray direction in world space
+    const Vector3 D = p - o;
+
+    return Ray(o, D);
+}
+
+const Image& Composer::image() const
+{
+    return image_;
+}
