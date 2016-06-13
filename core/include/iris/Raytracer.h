bc1ebf9233c0f4aba3441f5996845e2608b16fca
Raoul Harel <raoul.harel@gmail.com>
2016-06-13 19:54:38 +0200

    added Raytracer class

---
+++ core/include/iris/Raytracer.h
@@ -0,0 +1,32 @@
+#ifndef IRIS_RAYTRACER_H
+#define IRIS_RAYTRACER_H
+/**
+ * @author Raoul Harel
+ * @url github.com/rharel/cpp-raytracer
+ *
+ * Raytracer abstract class.
+ */
+
+
+#include <iris/Ray.h>
+#include <iris/Scene.h>
+#include <iris/vector_types.h>
+
+
+namespace iris
+{
+    /**
+     * Traces rays through the scene.
+     */
+    class Raytracer
+    {
+        public:
+        /**
+         * Traces a ray through the scene.
+         */
+        virtual Vector3 trace(const Ray& ray, const Scene& scene) const = 0;
+    };
+}
+
+
+#endif  // IRIS_RAYTRACER_H
