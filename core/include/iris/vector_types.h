f6c33a0aecadec945643fdb8a38071cbeb4de75c
Raoul Harel <raoul.harel@gmail.com>
2016-06-14 22:52:45 +0200

    added quaternion type

--- core/include/iris/vector_types.h
+++ core/include/iris/vector_types.h
@@ -11,6 +11,7 @@
 
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 
 namespace iris
@@ -26,6 +27,8 @@ namespace iris
     typedef glm::vec2 Vector2;
     typedef glm::vec3 Vector3;
     typedef glm::vec4 Vector4;
+
+    typedef glm::quat Quaternion;
 }
 
 
