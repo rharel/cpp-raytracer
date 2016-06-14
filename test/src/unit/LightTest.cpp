727c572ef98991cc700de9e335f841614b9404c0
Raoul Harel <raoul.harel@gmail.com>
2016-06-14 22:53:31 +0200

    updated light test to work with mock lights

--- test/src/unit/LightTest.cpp
+++ test/src/unit/LightTest.cpp
@@ -6,14 +6,24 @@
 using namespace iris;
 
 
+class MockLight : public Light
+{
+    public:
+    MockLight() 
+        : Light() {}
+    MockLight(const Vector3& color, const float intensity)
+        : Light(color, intensity) {}
+    void random_on_surface(Vector3&, Vector3&) const override {}
+    float surface_area() const override { return 1.0f; }
+};
 TEST_CASE("light", "[light]")
 {
     const Vector3 color(0.5f);
     const float intensity = 0.8f;
-    const Light a;
-    const Light b(color, intensity);
-    const Light c(Vector3(10.0f), intensity);
-    const Light d(Vector3(-10.0f), intensity);
+    const MockLight a;
+    const MockLight b(color, intensity);
+    const MockLight c(Vector3(10.0f), intensity);
+    const MockLight d(Vector3(-10.0f), intensity);
 
     REQUIRE(a.color() == Vector3(1.0f));
     REQUIRE(a.intensity() == 1.0f);
