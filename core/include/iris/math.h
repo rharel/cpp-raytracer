e29feac898d33ba815c879c81d6815a76eb1b075
Raoul Harel <raoul.harel@gmail.com>
2016-06-14 16:16:10 +0200

    added gaussian evaluation methods

--- core/include/iris/math.h
+++ core/include/iris/math.h
@@ -19,10 +19,77 @@ namespace iris
     /**
      * Solves the quadratic equation ax^2 + bx + c = 0
      */
-    bool solve_quadratic(
-        
+    bool solve_quadratic
+    (
         float a, float b, float c,
-        float& x0, float& x1);
+        float& x0, float& x1
+    );
+
+    /**
+     * Computes the average value of a collection.
+     */
+    template <typename Value, typename Iterator>
+    Value average(Iterator begin, Iterator end)
+    {
+        Value sum = Value(0);
+        size_t n = 0;
+        for (Iterator it = begin; it != end; ++it)
+        {
+            sum += *it;
+            ++n;
+        }
+            
+        return sum / static_cast<float>(n);
+    };
+    /**
+     * Computes the weighted average value of a collection.
+     */
+    template
+    <
+        typename Value, 
+        typename ValueIterator,
+        typename WeightIterator
+    >
+    Value weighted_average
+    (
+        ValueIterator value_begin, 
+        ValueIterator value_end,
+        WeightIterator weight_begin)
+    {
+        Value sum_values = Value(0);
+        float sum_weights = 0.0f;
+
+        WeightIterator weight_it = weight_begin;
+        for (ValueIterator value_it = value_begin; 
+             value_it != value_end; 
+             ++value_it, ++weight_it)
+        {
+            sum_values += *weight_it * *value_it;
+            sum_weights += *weight_it;
+        }
+            
+        return sum_values / sum_weights;
+    };
+
+    /**
+     * Evaluates 2D gaussian.
+     */
+    float gaussian_2d
+    (
+        float x, float y,
+        float x0, float y0,
+        float sigma_x, float sigma_y,
+        float A
+    );
+    /**
+     * Evalutes 2D isotrpic gaussian.
+     */
+    float gaussian_2d_isotropic
+    (
+        float x, float y,
+        float x0, float y0,
+        float sigma
+    );
 }
 
 
--- test/src/unit/mathTest.cpp
+++ test/src/unit/mathTest.cpp
@@ -1,6 +1,7 @@
 #include "../../lib/catch.hpp"
 
 #include <iris/math.h>
+#include <iris/vector_types.h>
 
 
 using namespace iris;
@@ -31,4 +32,70 @@ TEST_CASE("solve quadratic", "[math][solve-quadratic]")
         REQUIRE(x0 ==  1.0f);
         REQUIRE(x1 == -1.0f);
     }
+}
+TEST_CASE("average", "[math][average]")
+{
+    float reals[3] { 1.0f, 2.0f, 3.0f };
+    REQUIRE(average<float>(&reals[0], &reals[3]) == 2.0f);
+
+    Vector3 vectors[3] 
+    { 
+        Vector3(1, 2, 3),
+        Vector3(3, 1, 2),
+        Vector3(2, 3, 1)
+    };
+    const Vector3 avg = average<Vector3>(&vectors[0], &vectors[3]);
+    REQUIRE(avg == Vector3(2));
+}
+TEST_CASE("weighted average", "[math][average-weighted]")
+{
+    const float reals[3] { 1.0f, 2.0f, 3.0f };
+    const float weights[3] { 1.0f, 2.0f, 3.0f };
+    REQUIRE(weighted_average<float>
+    (
+        &reals[0], &reals[3], 
+        &weights[0]
+    ) == 14.0f / 6.0f);
+
+    Vector3 vectors[3] 
+    { 
+        Vector3(1, 2, 3),
+        Vector3(3, 1, 2),
+        Vector3(2, 3, 1)
+    };
+    const Vector3 avg = weighted_average<Vector3>
+    (
+        &vectors[0], &vectors[3], 
+        &weights[0]
+    );
+    const Vector3 expected
+    (
+        13.0f / 6.0f, 
+        13.0f / 6.0f, 
+        10.0f / 6.0f
+    );
+    REQUIRE(avg == expected);
+}
+
+TEST_CASE("gaussian-2d", "[math][gaussian][gaussian-2d]")
+{
+    const float x = 0.01f , y = 0.02f, 
+                x0 = 3, y0 = 4,
+                sigma_x = 1.1f, sigma_y = 2.1f,
+                A = 0.5f;
+    const float expected = 0.002063468f;
+    
+    REQUIRE(gaussian_2d(x, y, x0, y0, sigma_x, sigma_y, A) - 
+            expected < 0.000001f);
+}
+TEST_CASE("gaussian-2d-isotropic", "[math][gaussian][gaussian-2d-isotropic]")
+{
+    const float x = 0.01f , y = 0.02f, 
+                x0 = 3, y0 = 4,
+                sigma = 1.1f,
+                A = 1 / (constant::m_2_pi * sigma);
+    const float expected = 0.000005167801f;
+    
+    REQUIRE(gaussian_2d_isotropic(x, y, x0, y0, sigma) - 
+            expected < 0.000001f);
 }
