42ca70f68f46f9b1f58141ffa957e61dbd1db4d4
Raoul Harel <raoul.harel@gmail.com>
2016-06-15 18:37:05 +0200

    added ray bounce to materials, brdf methods are now pure virtual

--- core/include/iris/Material.h
+++ core/include/iris/Material.h
@@ -23,7 +23,7 @@ namespace iris
         /**
          * Evaluates the BRDF's integral.
          */
-        virtual Vector3 brdf() const;
+        virtual Vector3 brdf() const = 0;
         /**
          * Evaluates the BRDF for given incoming/outgoing 
          * ray directions.
@@ -37,7 +37,19 @@ namespace iris
         (
             const Vector3& Wi, 
             const Vector3& Wo
-        ) const;
+        ) const = 0;
+
+        /**
+         * Bounces a ray to a new random direction.
+         *
+         * @param      Wi  Incoming ray direction.
+         * @param[out] pdf Probability of chosen outgoing direction.
+         *
+         * @returns Outgoing direction.
+         *
+         * @note Assumes normal at contact point is N = (0, 1, 0).
+         */
+        virtual Vector3 bounce(const Vector3& Wi, float& pdf) const = 0;
 
         bool operator==(const Material& other) const;
         bool operator!=(const Material& other) const;
--- core/src/Material.cpp
+++ core/src/Material.cpp
@@ -4,15 +4,6 @@
 using namespace iris;
 
 
-Vector3 Material::brdf() const
-{
-    return Vector3(0);
-}
-Vector3 Material::brdf(const Vector3&, const Vector3&) const
-{
-    return Vector3(0);
-}
-
 bool Material::operator==(const Material& other) const
 {
     return this == &other;
