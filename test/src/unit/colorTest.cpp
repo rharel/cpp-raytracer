6ee8de941d361d04e5601f956fd9448ef13393f0
Raoul Harel <raoul.harel@gmail.com>
2016-06-15 18:57:07 +0200

    rename hex -> from_hex

--- test/src/unit/colorTest.cpp
+++ test/src/unit/colorTest.cpp
@@ -9,7 +9,7 @@ using namespace iris::color;
 
 TEST_CASE("color", "[color][hex]")
 {
-    REQUIRE(hex(0xFF0000) == Vector3(1, 0, 0));
-    REQUIRE(hex(0x00FF00) == Vector3(0, 1, 0));
-    REQUIRE(hex(0x0000FF) == Vector3(0, 0, 1));
+    REQUIRE(from_hex(0xFF0000) == Vector3(1, 0, 0));
+    REQUIRE(from_hex(0x00FF00) == Vector3(0, 1, 0));
+    REQUIRE(from_hex(0x0000FF) == Vector3(0, 0, 1));
 }
